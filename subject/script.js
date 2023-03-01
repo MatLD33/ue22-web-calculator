4b68b1f10068a1aa568d930b56901dd34c5c2d6b
Matteo Leturcq--Daligaux <matteo.leturcq--daligaux@etu.minesparis.psl.eu>
2023-03-01 16:38:40 +0100

    Worki,g display

--- subject/script.js
+++ subject/script.js
@@ -3,3 +3,31 @@
 
 // # START EDITING YOUR JAVASCRIPT HERE
 // ===============
+
+let keys = (document.getElementsByClassName("calculator__keys"))[0]
+let display = document.getElementsByClassName("calculator__display")[0]
+
+function add(x, y) {
+    return (x + y)
+}
+
+for (let button of keys.children) {
+    let content = button.textContent
+    button.addEventListener("click", () => {
+        console.log(content)
+
+        if (display.textContent == 0) {
+            display.textContent = content
+        }
+        else {
+            display.textContent += content
+        }
+
+
+
+
+        if (content == "AC") {
+            display.textContent = 0
+        }
+    })
+}
